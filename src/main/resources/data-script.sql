c06b4b5f525fb6be372584daf7461772e46ad73c
nayelli <nayelli.illescas@gmail.com>
2023-10-22 20:19:01 -0500

    add mysql cloud

--- src/main/resources/data-script.sql
+++ src/main/resources/data-script.sql
@@ -1,6 +1,6 @@
-insert into role values(1, 'ADMINISTRADOR');
-insert into role values(2, 'USUARIO');
+insert into Role values(1, 'ADMINISTRADOR');
+insert into Role values(2, 'USUARIO');
 
-insert into user values (1, 'admin@admin', 'admin', '$2a$10$uhnIWWDa.PrGOqpoLda6fuevn/RTJquP5GsGLhjVDpaNg64Ymf1Ee', 'admin');
+insert into User values (1, 'admin@admin', 'admin', '$2a$10$uhnIWWDa.PrGOqpoLda6fuevn/RTJquP5GsGLhjVDpaNg64Ymf1Ee', 'admin');
 
-insert into user_role values (1, 1);
+insert into User_Role values (1, 1);
